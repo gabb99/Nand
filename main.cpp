9586ba9f924936c6ab56ce9f761516b1fc60a5ac
Gabriel Beauchemin <gbeauchemin@gmail.com>
2018-12-20 19:40:10 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NAND
+//
+//  Created by Gabriel Beauchemin on 2018-12-20.
+//  Copyright © 2018 Gabriel Beauchemin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
