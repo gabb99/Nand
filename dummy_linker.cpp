f9a0c027923700700f7ada33bfbc02d7f44339f7
Gabriel Beauchemin <>
2023-01-29 14:38:13 -0500

    Use cmake/vcpkg/ninja

---
+++ dummy_linker.cpp
@@ -0,0 +1,11 @@
+//
+//  dummy_linker.cpp
+//  NAND
+//
+//  Created by Gabriel Beauchemin on 2018-12-23.
+//  Copyright © 2018 Gabriel Beauchemin. All rights reserved.
+//
+
+void dummy_linker()
+{
+}
