215541eeaf848289161c4b2fc4c2b760c3840ece
Gabriel Beauchemin <gbeauchemin@hme.com>
2023-02-03 15:13:52 -0500

    Some fixes for x86

--- adder.hpp
+++ adder.hpp
@@ -19,7 +19,7 @@ class adder_t
 	{
 		and_t<2> m_and;
 		xor_t<2> m_xor;
-		
+
 	public:
 		adder_1_t() {}
 
@@ -47,54 +47,54 @@ class adder_t
 	};
 
 	std::array<adder_1_t, N> m_adders;
-	
+
 public:
 	adder_t() {}
-	
+
 	void attach(const std::function<void(bool)>& cb, unsigned n = 0)
 	{
-		m_ands[n].attach(cb);
+		m_adders[n].attach(cb);
 	}
-	
+
 	void enable(bool value)
 	{
-		for (auto&& n : m_ands)
+		for (auto&& n : m_adders)
 			n.in(value, 1);
 	}
-	
+
 	void in(const std::bitset<N>& b)
 	{
 		for (unsigned i = 0; i < N; i++)
-			m_ands[i].in(b[i], 0);
+			m_adders[i].in(b[i], 0);
 	}
-	
+
 	void in(const std::initializer_list<bool>& in)
 	{
 		assert(in.size() == N);
-		
+
 		for (unsigned i = 0; i < N; i++)
-			m_ands[i].in(in.begin()[i], 0);
+			m_adders[i].in(in.begin()[i], 0);
 	}
-	
+
 	void in(bool value, unsigned n)
 	{
-		assert(n < m_ands.size());
-		m_ands[n].in(value, 0);
+		assert(n < m_adders.size());
+		m_adders[n].in(value, 0);
 	}
-	
+
 	std::bitset<N> out() const
 	{
 		std::bitset<N> n;
-		
+
 		for (unsigned i = 0; i < N; i++)
 			n.set(i, out(i));
-		
+
 		return n;
 	}
-	
+
 	bool out(unsigned n) const
 	{
-		return m_ands[n].out();
+		return m_adders[n].out();
 	}
 };
 
